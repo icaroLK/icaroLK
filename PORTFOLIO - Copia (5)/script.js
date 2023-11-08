9975be989a57c0cd20aeb96c4dc2b9dd363ae34a
Ícaro Kuchanovicz <icarolima.k7@gmail.com>
2023-11-07 22:09:07 -0300

    oi

--- PORTFOLIO - Copia (5)/script.js
+++ PORTFOLIO - Copia (5)/script.js
@@ -4,6 +4,7 @@
 function openMenu(){
     var $nav = document.querySelector('nav');
     var $elements = document.querySelectorAll('nav ul li');
+    var $head = document.querySelector('header');
     
     $elements.forEach(function(element) {
         element.classList.toggle('texto');
